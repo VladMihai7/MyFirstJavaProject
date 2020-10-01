ab474854b69afdb266a9b81d13833ec2199c5912
VladMihai7 <68895563+VladMihai7@users.noreply.github.com>
2020-10-01 16:22:07 +0300

    Add files via upload

---
+++ MyFirstProject/src/Variabile.java
@@ -0,0 +1,31 @@
+public class Variabile {
+    public static void main(String[] args) {
+        int first = 1;
+        int second = 2;
+        int adding = first + second;
+
+        System.out.println(adding);
+    }
+
+
+    }
+ class Variabile2 {
+    static int one = 123;
+
+     public static void main(String[] args) {
+         final int finalVar = 10;
+         int finalAdding = 2 + one +finalVar;
+         System.out.println(finalAdding);
+     }
+ }
+
+
+ class Test {
+
+    public static void main(String args[]) {
+        Integer x = 5; //
+        x =  x + 10;
+        System.out.println(x);
+    }
+}
+
